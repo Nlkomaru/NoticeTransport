52ba35866c7fb74b5c15673eeaa1238a75d598c1
Nikomaru <nikomaru@nikomaru.dev>
2022-07-29 17:09:49 +0900

    Fixed to be able to use commands correctly.

--- src/main/kotlin/com/noticemc/noticetransport/velocity/NoticeTransport.kt
+++ src/main/kotlin/com/noticemc/noticetransport/velocity/NoticeTransport.kt
@@ -1,10 +1,13 @@
 package com.noticemc.noticetransport.velocity
 
 import cloud.commandframework.annotations.*
-import cloud.commandframework.execution.CommandExecutionCoordinator
+import cloud.commandframework.execution.AsynchronousCommandExecutionCoordinator
+import cloud.commandframework.kotlin.coroutines.annotations.installCoroutineSupport
 import cloud.commandframework.meta.SimpleCommandMeta
 import cloud.commandframework.velocity.CloudInjectionModule
 import cloud.commandframework.velocity.VelocityCommandManager
+import com.github.shynixn.mccoroutine.velocity.SuspendingPluginContainer
+import com.github.shynixn.mccoroutine.velocity.registerSuspend
 import com.google.inject.*
 import com.noticemc.noticetransport.velocity.commands.TransportCommand
 import com.noticemc.noticetransport.velocity.event.PlayerLeftEvent
@@ -12,6 +15,7 @@ import com.noticemc.noticetransport.velocity.files.Config
 import com.velocitypowered.api.command.CommandSource
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
+import com.velocitypowered.api.plugin.PluginContainer
 import com.velocitypowered.api.plugin.annotation.DataDirectory
 import com.velocitypowered.api.proxy.ProxyServer
 import java.nio.file.Path
@@ -23,7 +27,11 @@ class NoticeTransport {
     lateinit var injector: Injector
 
     @Inject
-    fun noticeTransport(server: ProxyServer, logger: Logger, @DataDirectory dataDirectory: Path) {
+    lateinit var pluginContainer: PluginContainer
+
+    @Inject
+    fun noticeTransport(server: ProxyServer, logger: Logger, @DataDirectory dataDirectory: Path ,suspendingPluginContainer: SuspendingPluginContainer) {
+        suspendingPluginContainer.initialize(this)
         Companion.logger = logger
         Companion.server = server
         Companion.dataDirectory = dataDirectory
@@ -31,14 +39,15 @@ class NoticeTransport {
 
     @Subscribe
     fun onProxyInitialization(event: ProxyInitializeEvent) {
-        server.eventManager.register(this, PlayerLeftEvent())
-        setCommand()
         Config.load()
+        server.eventManager.registerSuspend(this, PlayerLeftEvent())
+        setCommand()
+
     }
 
     private fun setCommand() {
         val childInjector: Injector = this.injector.createChildInjector(CloudInjectionModule(CommandSource::class.java,
-            CommandExecutionCoordinator.simpleCoordinator(),
+            AsynchronousCommandExecutionCoordinator.newBuilder<CommandSource>().build(),
             java.util.function.Function.identity(),
             java.util.function.Function.identity()))
         val commandManager: VelocityCommandManager<CommandSource> =
@@ -46,7 +55,7 @@ class NoticeTransport {
 
         val annotationParser: AnnotationParser<CommandSource> = AnnotationParser(commandManager, CommandSource::class.java) {
             SimpleCommandMeta.empty()
-        }
+        }.installCoroutineSupport()
 
         annotationParser.parse(TransportCommand())
     }
